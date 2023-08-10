c171fc9c6f5c3a44cc53ae37c1e780d12fc4793e
ZHumay <114071477+ZHumay@users.noreply.github.com>
2023-08-10 19:49:24 +0400

    Update Shop.jsx

--- client/src/Pages/Shop/Shop.jsx
+++ client/src/Pages/Shop/Shop.jsx
@@ -1,129 +1,160 @@
-import React, { useEffect , useState } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import "../Shop/shop.css"
-import BlogPostCardHome from '../../Components/BlogPostCardHome/BlogPostCardHome';
-import { useActiveUserContext } from '../../hooks/useActiveUserContext';
-import { usePostsContext } from '../../hooks/usePostsContext';
-import { useUsersContext } from '../../hooks/useUsersContext';
-import { useCommentsContext } from '../../hooks/useCommentsContext';
-import Footer from '../../Components/Footer/Footer';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import "../Shop/shop.css";
+import BlogPostCardHome from "../../Components/BlogPostCardHome/BlogPostCardHome";
+import { useActiveUserContext } from "../../hooks/useActiveUserContext";
+import { usePostsContext } from "../../hooks/usePostsContext";
+import { useUsersContext } from "../../hooks/useUsersContext";
+import { useCommentsContext } from "../../hooks/useCommentsContext";
+import Footer from "../../Components/Footer/Footer";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import CachedIcon from "@mui/icons-material/Cached";
 import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import PaymentIcon from "@mui/icons-material/Payment";
-import {
-  Button
-} from "@mui/material";
+import { Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import FilterListIcon from "@mui/icons-material/FilterList";
 function Shop() {
   const navigate = useNavigate();
-  const {comments, dispatchComments} = useCommentsContext();
-  const {posts, dispatch} = usePostsContext(); 
-  const {activeUser, dispatchActiceUser} = useActiveUserContext();
-  const {users, dispatchUsers} = useUsersContext();
+  const { comments, dispatchComments } = useCommentsContext();
+  const { posts, dispatch } = usePostsContext();
+  const { activeUser, dispatchActiceUser } = useActiveUserContext();
+  const { users, dispatchUsers } = useUsersContext();
   const [allPosts, setAllPosts] = useState();
   const [cate, setCate] = useState();
-  const [gender,setgender]=useState()
+  const [gender, setgender] = useState();
 
-
-  const filterCategory = (category) =>{
+  const filterCategory = (category) => {
     setCate(category);
-    if(category === "all"){
+    if (category === "all") {
       setAllPosts(posts);
-    }
-    else{
+    } else {
       const tempArray = posts.filter((post) => post.category === category);
       setAllPosts(tempArray);
     }
-   
-  }
+  };
 
-  const filterGender=(gender)=>{
-     setgender(gender);
-     const tempArr=posts.filter((post)=>post.gender===gender)
-     setAllPosts(tempArr)
-  }
+  const filterGender = (gender) => {
+    setgender(gender);
+    const tempArr = posts.filter((post) => post.gender === gender);
+    setAllPosts(tempArr);
+  };
 
-
-  useEffect(()=>{
+  useEffect(() => {
     console.log(activeUser);
-      navigate("/shop", {replace : true});
+    navigate("/shop", { replace: true });
 
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/api/posts/posts");
 
-        const fetchPosts = async () =>{
-          try{
-            const res = await axios.get("/api/posts/posts");
-
-            if(res.status === 200){
-              setAllPosts(res.data.posts);
-              dispatch({type :"SET_POSTS", payload : res.data.posts.reverse()})
-            }
-            else{
-              console.log(res.data.msg)
-            }
-
-          }catch(error){
-            console.log(error.message);
-          }
+        if (res.status === 200) {
+          setAllPosts(res.data.posts);
+          dispatch({ type: "SET_POSTS", payload: res.data.posts.reverse() });
+        } else {
+          console.log(res.data.msg);
+        }
+      } catch (error) {
+        console.log(error.message);
       }
+    };
 
-   
-
-     fetchPosts();
- 
-
-  },[dispatch])
-
+    fetchPosts();
+  }, [dispatch]);
 
   return (
     <>
-  <div className='shop'>
+      {activeUser ? (
+        <div className="shop">
+          <div className="home">
+            <div className="home_wrapper">
+              <div className="leftHome">
+                {allPosts?.length === 0 ? (
+                  <p className="posts_not_found">
+                    No Products of {cate} Category
+                  </p>
+                ) : (
+                  allPosts?.map((post) => {
+                    return <BlogPostCardHome key={post._id} post={post} />;
+                  })
+                )}
+              </div>
 
-
-<div className='home'>
-
-<div className="home_wrapper">
-  <div className="leftHome">
-        {
-      
-
-        allPosts?.length === 0 ? <p className='posts_not_found'>No Products of {cate} Category</p>: 
-        allPosts?.map((post)=>{
-          return <BlogPostCardHome key={post._id} post={post} />
-        })
-         
-       
-      }
-  </div>
-
-    <div className="rightHome">
-       <h2 className='catogory_heading'>Categories</h2>
-       <div className="categories_wrapper">
-            <span className='category_item' onClick={()=> filterCategory("tshirt")}>Tshirts</span>
-            <span className='category_item' onClick={()=> filterCategory("dress")}>Dresses</span>
-            <span className='category_item' onClick={()=> filterCategory("blazer")}>Blazers</span>
-            <span className='category_item' onClick={()=> filterCategory("jean")}>Jeans</span>
-            <span className='category_item' onClick={()=> filterCategory("coat")}>Coats</span>
-            <span className='category_item' onClick={()=> filterCategory("short")}>Shorts</span>
-            <span className='category_item' onClick={()=> filterCategory("skirt")}>Skirts</span>
-            <span className='category_item' onClick={()=> filterCategory("sweater")}>Sweaters</span>
-            <span className='category_item' onClick={()=> filterCategory("shoes")}>Shoes</span>
-            <span className='category_item' onClick={()=> filterCategory("bag")}>Bags</span>
-       </div>
-  </div>
-  
-  
-</div>
-
-</div>
-</div>
-<Footer/>
-</>
-
-)
+              <div className="rightHome">
+                <h2 className="catogory_heading">Categories</h2>
+                <div className="categories_wrapper">
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("tshirt")}
+                  >
+                    Tshirts
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("dress")}
+                  >
+                    Dresses
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("blazer")}
+                  >
+                    Blazers
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("jean")}
+                  >
+                    Jeans
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("coat")}
+                  >
+                    Coats
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("short")}
+                  >
+                    Shorts
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("skirt")}
+                  >
+                    Skirts
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("sweater")}
+                  >
+                    Sweaters
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("shoes")}
+                  >
+                    Shoes
+                  </span>
+                  <span
+                    className="category_item"
+                    onClick={() => filterCategory("bag")}
+                  >
+                    Bags
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        navigate("/login")
+      )}
+      <Footer />
+    </>
+  );
 }
 
-
-export default Shop
+export default Shop;
